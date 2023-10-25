eabe0f958e2331757b6f8c423b5317a599f0b9a1
FrenchDoomer <montfrond.timothe.pro@gmail.Com>
2023-10-25 17:00:05 +0200

    Popup working !

--- easyai/webapp.py
+++ easyai/webapp.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 from easyai import core
+from unittest import mock
+from werkzeug._internal import _log
+import webbrowser, flask.cli, os
+
 
 class Webapp():
     def __init__(self, system_info):
@@ -21,4 +25,29 @@ class Webapp():
         def exit(*self):
             return render_template("exit.html")
 
-        app.run(host="0.0.0.0", port=system_info['flask_port'], debug=True)
+        @app.route("/close-server", methods=["POST"])
+        def close_server():
+            core.main.cli_print("Server closed", type="alert")
+            os._exit(0)
+            return "Closing server"
+
+        def on_startup(*self):
+            port = system_info['flask_port']
+            core.main.cli_print(f"Launched at: http://127.0.0.1:{port}")
+            if system_info['server_mode'] == False:
+                webbrowser.open_new_tab(f"http://127.0.0.1:{port}")
+            elif system_info['server_mode'] == True:
+                core.main.cli_print("You are running in server mode, see documentation: https://google.com", "warning")
+            else:
+                core.main.cli_print("Can't etablish if you are in server mode.", type="alert")
+            if system_info['debug_mode'] == False:
+                pass
+            elif system_info['debug_mode'] == True:
+                core.main.cli_print("You are running in debug mode, see documentation: https://google.com", "warning")
+            else:
+                core.main.cli_print("Can't etablish if you are in debug mode.", type="alert")
+        with mock.patch('werkzeug.serving._log') as mocked:
+            # patch the logger object and replace with own logger
+            flask.cli.show_server_banner = lambda *args: None
+            mocked.side_effect = on_startup()
+            app.run(host="0.0.0.0", port=system_info['flask_port'], debug=system_info['debug_mode'])
--- install-requirements.bat
+++ install-requirements.bat
@@ -2,8 +2,7 @@
 REM ====================================================
 REM ====================================================
 set PYTHON= "E:\EasyAI\.venv\Scripts\python.exe"
-set COMMANDLINE_ARGS= -s -d
 REM ====================================================
 REM ====================================================
 if not defined PYTHON (set PYTHON=python)
-%PYTHON% run.py %COMMANDLINE_ARGS%
+%PYTHON% -m pip install -r requirements.txt
